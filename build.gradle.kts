e9b456604531ec9a57103420240e83beb52d13e3
Sean Proctor <sproctor@gmail.com>
2023-02-08 10:05:26 -0500

    initial comit

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+    id("com.google.devtools.ksp")
+}
+
+group "com.example"
+version "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    js(IR) {
+        browser {
+            testTask {
+                testLogging.showStandardStreams = true
+                useKarma {
+                    useChromeHeadless()
+                    useFirefox()
+                }
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+                implementation("me.tatarka.inject:kotlin-inject-runtime:0.5.1")
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+    }
+}
+
+dependencies {
+    add("kspJs", "me.tatarka.inject:kotlin-inject-compiler-ksp:0.5.1")
+}
